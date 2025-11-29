849cbae4a1a9d101a5d6b571953b72ef2a505a68
IrynaKhonina <irinahonina@yahoo.com>
2025-11-29 17:18:11 +0300

    appsidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,11 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import {Navbar} from "@/components/Navbar";
+import {ThemeProvider} from "@/components/providers/ThemeProvider";
+import {SidebarProvider} from "@/components/ui/sidebar";
+import AppSidebar from "@/components/AppSidebar";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -22,13 +27,26 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" suppressHydrationWarning>
+        <body
+            className={`${geistSans.variable} ${geistMono.variable} antialiased flex`}
+        >
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+        >
+            <SidebarProvider >
+                <AppSidebar />
+                <main className="w-full">
+                    <Navbar />
+                    <div className="px-4">{children}</div>
+                </main>
+            </SidebarProvider>
+        </ThemeProvider>
+        </body>
+        </html>
+    );
 }
