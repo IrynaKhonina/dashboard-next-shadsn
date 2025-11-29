4eb65f9a2798bc3a0e614facac873a32797b9661
IrynaKhonina <irinahonina@yahoo.com>
2025-11-29 17:19:29 +0300

    appsidebar

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -60,7 +60,7 @@ const AppSidebar = () => {
                         <SidebarMenuButton asChild>
                             <Link href="/">
                                 <Image src="/logo.svg" alt="logo" width={20} height={20} />
-                                <span>Lama Dev</span>
+                                <span>Iryna Honina</span>
                             </Link>
                         </SidebarMenuButton>
                     </SidebarMenuItem>
